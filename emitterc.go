008f9b5bff01a86b8d9a0f5475c185c5de2a7a5e
tdakkota <tanc13@yandex.ru>
2022-08-11 17:20:32 +0300

    fix: emit break after folded scalar start

--- emitterc.go
+++ emitterc.go
@@ -1926,10 +1926,15 @@ func yaml_emitter_write_folded_scalar(emitter *yaml_emitter_t, value []byte) boo
 	if !yaml_emitter_write_block_scalar_hints(emitter, value) {
 		return false
 	}
+	no_comment := len(emitter.line_comment) == 0
 	if !yaml_emitter_process_line_comment(emitter) {
 		return false
 	}
-
+	if no_comment {
+		if !put_break(emitter) {
+			return false
+		}
+	}
 	// emitter.indention = true
 	emitter.whitespace = true
 
