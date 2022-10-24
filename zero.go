a35a069b9bd27cc91d2b090e84941e2fa564e978
tdakkota <tanc13@yandex.ru>
2022-10-24 11:54:18 +0300

    fix: use `IsZero` method to check if value has zero value
    Now zero-value arrays are handled correctly.
    Also, it simpler and probably faster.

--- zero.go
+++ zero.go
@@ -19,22 +19,6 @@ func isZero(v reflect.Value) bool {
 		return z.IsZero()
 	}
 	switch kind {
-	case reflect.String:
-		return v.String() == ""
-	case reflect.Interface, reflect.Ptr:
-		return v.IsNil()
-	case reflect.Slice:
-		return v.Len() == 0
-	case reflect.Map:
-		return v.Len() == 0
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		return v.Int() == 0
-	case reflect.Float32, reflect.Float64:
-		return v.Float() == 0
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
-		return v.Uint() == 0
-	case reflect.Bool:
-		return !v.Bool()
 	case reflect.Struct:
 		vt := v.Type()
 		for i := v.NumField() - 1; i >= 0; i-- {
@@ -46,6 +30,11 @@ func isZero(v reflect.Value) bool {
 			}
 		}
 		return true
+	case reflect.Slice, reflect.Map:
+		// `reflect.Value.IsZero` checks for nil, but for backward compatibility
+		// we treat nil and empty slice/map as zero.
+		return v.Len() == 0
+	default:
+		return v.IsZero()
 	}
-	return false
 }
